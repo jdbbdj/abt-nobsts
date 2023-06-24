dbe3c3537e60900ffb2a369fc318455b73390497
James Daryl Bertiz <jamesdarylbertiz@gmail.com>
2023-06-24 15:54:34 +0800

    Typescript Setup & Everyday Types

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # abt-nobsts
-## Hey
+# Mac Shortcuts as helpers for typescript
+
+Identify the type by:
+> Highlight variable + Command K + Command I identify the data type of highlighted variable
+
+What to do?
+> copy the colon beside the variable to ensure the data type is the one that's what you needed
---
+++ basics.ts
@@ -0,0 +1,23 @@
+let userName: string = "Jack"
+let hasLoggedIn: boolean = true
+
+userName += " Herrington"
+
+console.log(userName)
+
+let myNumber: number = 10;
+
+
+//regex
+let myRegex: RegExp = /foo/;
+
+
+//arrays
+const names: string[] = userName.split(" ")
+
+//generic type array
+const myValues: Array<string> = names
+
+
+//number generic
+const anotherValues: Array<number> = [1,2,3]
