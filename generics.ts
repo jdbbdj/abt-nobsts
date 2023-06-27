fec9115cbe4147b6037e0ed87be20362c5e15137
James Daryl Bertiz <jamesdarylbertiz@gmail.com>
2023-06-28 01:24:58 +0800

    Second Example Pokemon Ranker

--- generics.ts
+++ generics.ts
@@ -21,3 +21,47 @@ const [users,setUsers]=genericUseState<string|null>(null)
 console.log(users());
 setUsers("str");
 console.log(users());
+
+
+//ranker
+
+interface Rank<RankItem> {
+    item:RankItem;
+    rank:number;
+    
+}
+
+const ranker =<RankItem> (items:RankItem[],rank:(v:RankItem)=>number):RankItem[]=>{
+   
+    const ranks:Rank<RankItem>[] = items.map((item)=>({
+        item,
+        rank:rank(item)
+    }))
+
+    ranks.sort((a,b)=>b.rank-a.rank)
+
+    return ranks.map((rank)=>rank.item)
+}
+
+interface Pokemon{
+    name:string;
+    hp:number;
+    atk:number
+}
+
+const pokemon: Pokemon[] = [
+    {
+        name:"Bulbasaur",
+        hp:20,
+        atk:500
+    },
+    {
+        name: "Megasaur",
+        hp:50,
+        atk:450
+    }
+]
+
+const ranks = ranker(pokemon,({atk})=>atk);
+
+console.log(ranks)
