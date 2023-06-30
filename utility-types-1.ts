41a5d20ee61778af4d57a87e9bf5adb83d9fe746
James Daryl Bertiz <jamesdarylbertiz@gmail.com>
2023-07-01 00:02:06 +0800

    Utility Types Overrides: Using Partial as shortcuts for overriding parameters

--- utility-types-1.ts
+++ utility-types-1.ts
@@ -1,28 +1,38 @@
+//this is the typescript for overriding data as params
 
-
+//declare a type/interface
+//interface could be redeclared types couldnt be
+//type has equals
 
 interface MyUser {
     name:string;
     id:string;
+    //this is an optional type we could put ? as optional
     email?:string;
 }
 
-interface MyUserOptionals{
-    name?:string;
-    id?:string;
-    email?:string;
-}
 
-const merge = (user:MyUser,overrides:MyUserOptionals):MyUser=>{
-    return {
+//we forgot to declare all as optionals
+// interface OverridesOptionals {
+//     name?:string;
+//     id?:string;
+//     //this is an optional type we could put ? as optional
+//     email?:string;
+// }
+
+//now we make a very useful syntax in typescript
+//Partial
+
+const merge = (user:MyUser,overrides:Partial<MyUser>):MyUser=>{
+    //declaring arrow function in typescript
+    //will result error without returning anything
+
+    return ({
         ...user,
         ...overrides
-    }
+    })
 }
 
-console.log(merge({
-    name:"Jack",
-    id:"Foo",
-    email:"dontemail@dontemail.com"
-},{email:"dontemailssssssss@dontemail.com"}))
-
+console.log(merge({name:"James",id:"Tutorials",email:"typescript@gmail.com"},{
+    email:"thisisoverride@gmail.com"
+}))
